9c20dbdd65ba78dad68a820a2d75318be0a52c22
ashwinii19 <ashdagale@gmail.com>
2025-11-20 02:17:30 +0530

    Css changed for create survey, sidebar, recent survey

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { RouterLink, RouterLinkActive } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+
+// @Component({
+//   selector: 'app-sidebar',
+//   standalone: true,
+//   imports: [CommonModule, RouterLink, RouterLinkActive],
+//   templateUrl: './sidebar.html',
+//   styleUrls: ['./sidebar.css']
+// })
+// export class Sidebar {}
+import { Component, EventEmitter, Output, HostBinding } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
-  styleUrls: ['./sidebar.css']
+  styleUrls: ['./sidebar.css'],
+  imports: [
+    NgClass,        // <-- ADD THIS
+    RouterLink,
+    RouterLinkActive,
+    CommonModule
+  ]
 })
-export class Sidebar {}
+export class Sidebar {
+
+  @HostBinding('class.collapsed') hostCollapsed = false;
+
+  @Output() collapseChange = new EventEmitter<boolean>();
+
+  isCollapsed = false;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+    this.hostCollapsed = this.isCollapsed;   
+
+    this.collapseChange.emit(this.isCollapsed);
+  }
+
+  logout() {
+    localStorage.clear();
+    location.href = "/login";
+  }
+}

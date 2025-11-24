55ecbbcfdf026d5d76b09f98e1794dd14dec112a
ashwinii19 <ashdagale@gmail.com>
2025-11-24 11:48:41 +0530

    Dashboard Updated with filter

--- src/app/pages/pending-table/pending-table.ts
+++ src/app/pages/pending-table/pending-table.ts
@@ -1,19 +1,3 @@
-// import { Component, Input } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-
-// @Component({
-//   selector: 'app-pending-table',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './pending-table.html',
-//   styleUrls: ['./pending-table.css']
-// })
-// export default class PendingTable {
-//   // accept any[] so tpl can use p.name safely
-//   @Input() pending: any[] = [];
-// }
-
-
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
